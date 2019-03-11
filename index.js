2656b5ca7baa5fc979e16bb49b7dd6aee4473e92
Ryan Gellis <ryan.gellis@rmgmedia.com>
2019-03-11 01:00:30 -0400

    return wrapped component

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 export const TitanFramework = (WrappedComponent) => {
-  // return class extends React.Component
-  // {
-  //   constructor(props)
-  //   {
-  //     super(props);
-  //   }
-  //
-  //   render()
-  //   {
-  //     return <WrappedComponent {...this.props} />;
-  //   }
-  // };
+  return class extends React.Component
+  {
+    constructor(props)
+    {
+      super(props);
+    }
+
+    render()
+    {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
   return WrappedComponent;
 };
