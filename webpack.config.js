467f0a3437afb34753bf7b0cd21c0f6e8900a55b
Ryan Gellis <ryan.gellis@rmgmedia.com>
2019-03-13 10:36:26 -0400

    updated webpack config to excl dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|dist)/,
         use: {
           loader: 'babel-loader',
         }
