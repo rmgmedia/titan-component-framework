2df1874dab2b8b2d45d8999c4b5ca52b50d0a96c
Ryan Gellis <ryan.gellis@rmgmedia.com>
2019-03-13 12:22:42 -0400

    moved babelrc into webapck config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         exclude: /(node_modules|dist)/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread']
+          }
         }
       }
     ]
