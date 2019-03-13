48da0f77e99525863d7c7d9835fff4dd49fe85fe
Ryan Gellis <ryan.gellis@rmgmedia.com>
2019-03-13 19:03:06 -0400

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread']
+            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-modules-commonjs']
           }
         }
       }
