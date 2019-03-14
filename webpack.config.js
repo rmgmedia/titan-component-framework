63f182629ba8e144faa6d1241698b75eca4cbf6f
Ryan Gellis <ryan.gellis@rmgmedia.com>
2019-03-14 15:52:30 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   module: {
     rules: [
@@ -8,23 +6,9 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-modules-commonjs']
+          presets: ['@babel/preset-env']
         }
       }
     ]
-  },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react')
-    }
-  },
-  externals: {
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
-    }
   }
 };
